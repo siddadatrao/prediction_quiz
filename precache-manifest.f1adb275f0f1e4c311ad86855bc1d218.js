c6973e260a702e344bd764e588217ab8c0d6a454
Siddhartha <adatrao2@illinois.edu>
2020-08-07 17:36:00 -0500

    Updates

---
+++ precache-manifest.f1adb275f0f1e4c311ad86855bc1d218.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67327044a350aa174270e5b05d7676c5",
+    "url": "/prediction_quiz/index.html"
+  },
+  {
+    "revision": "32ff203de06b0d83e34c",
+    "url": "/prediction_quiz/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9508c5d84d507d75f2ef",
+    "url": "/prediction_quiz/static/js/2.bb84829c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/prediction_quiz/static/js/2.bb84829c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32ff203de06b0d83e34c",
+    "url": "/prediction_quiz/static/js/main.bb76e640.chunk.js"
+  },
+  {
+    "revision": "d3b57ef943902d3117c1",
+    "url": "/prediction_quiz/static/js/runtime-main.e882f9bf.js"
+  }
+]);
